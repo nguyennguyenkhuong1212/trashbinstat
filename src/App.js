688746398919f8c305f288cf6b643b2caaa53ff6
Khuong <s3924577@rmit.edu.vn>
2022-09-11 16:37:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import PieChart from './components/PieChart';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Trashbin 1.0";
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        TRASHBIN 1.0
       </header>
+      <div id="container">
+          <div id="chartLabel">
+              Statistics
+          </div>
+          <PieChart />
+      </div> 
+      <footer>
+        Money Heist &copy; 2022
+      </footer>
     </div>
   );
 }
